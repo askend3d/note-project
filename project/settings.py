145685117dbe7c0e02fa399ee8e8ec438fdd5557
Quatry <stop.mean1@gmail.com>
2024-02-01 00:22:30 +0300

    Fix

--- project/settings.py
+++ project/settings.py
@@ -134,6 +134,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ALLOWED_ORIGINS = [
-    'localhost:3000',
-    'localhost:5173',
+    'http://localhost:3000',
+    'http://localhost:5173',
 ]
