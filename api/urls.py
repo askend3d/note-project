f5c763deb1af27c0d00b288a999d10c5a7570969
quatry <stop.mean1@gmail.com>
2024-02-04 02:57:09 +0300

    Named endpoints in api/urls.py

--- api/urls.py
+++ api/urls.py
@@ -5,13 +5,13 @@ from api.views import NoteListCreateAPIView, NoteRetrieveUpdateDestroyAPIView, M
     ProfileGetPutAPI
 
 urlpatterns = [
-    path('notes', NoteListCreateAPIView.as_view()),
-    path('notes/<int:pk>', NoteRetrieveUpdateDestroyAPIView.as_view()),
+    path('notes', NoteListCreateAPIView.as_view(),name='notes_list_create'),
+    path('notes/<int:pk>', NoteRetrieveUpdateDestroyAPIView.as_view(),name='note_get_update_delete'),
 
     path('token', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/logout', TokenBlacklistView.as_view(), name='token_blacklist'),
 
     path('register', RegisterView.as_view(), name='auth_register'),
-    path('profile', ProfileGetPutAPI.as_view())
+    path('profile', ProfileGetPutAPI.as_view(), name='profile_get_put')
 ]
