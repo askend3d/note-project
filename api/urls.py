5777de5f7468e0907e09612e6900b52ca633686f
Quatry <stop.mean1@gmail.com>
2024-02-03 22:23:14 +0300

    Added blacklist jwt

--- api/urls.py
+++ api/urls.py
@@ -5,15 +5,15 @@ from api.views import NoteListCreateAPIView, NoteRetrieveUpdateDestroyAPIView, M
     getProfile, updateProfile, getNotes
 
 urlpatterns = [
-    path('notes',getNotes),
+    path('notes', NoteListCreateAPIView.as_view()),
     path('notes/<int:pk>', NoteRetrieveUpdateDestroyAPIView.as_view()),
 
     path('token', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/logout', TokenBlacklistView.as_view(), name='token_blacklist'),
+
     path('register', RegisterView.as_view(), name='auth_register'),
 
     path('profile', getProfile, name='profile'),
     path('profile/update', updateProfile, name='update-profile'),
-
-    path('logout', TokenBlacklistView.as_view(), name='token_blacklist'),
 ]
